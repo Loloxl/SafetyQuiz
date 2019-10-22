bc9a88617e3416cc6f872154f1c667dcea8a20f7
Felipe Valencia <fvalencia@vc-soft.com>
2019-10-22 11:08:25 -0500

    Set basic data structure

--- quiz.py
+++ quiz.py
@@ -1,2 +1,17 @@
-print("Say hello")
-     
+name = ""
+score = 0
+questions = {"Q1:":
+                 {"A:": "??",
+                  "B:": "??",
+                  "C:": "??"},
+             "Q2:":
+                 {"A:": "??"}}
+
+for question in questions:
+    print("\n",question)
+    answers = questions[question]
+    for answer in answers:
+        print(answer,answers[answer])
+
+
+#def ask(question_num):
